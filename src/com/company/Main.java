e85f4a7e1d6714c2a3a0310c963608209e5f32d7
Zigmas <zambrazaitis@gmail.com>
2018-01-30 14:49:13 +0200

    uzduotis2

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // 1.2.​ Parašyti programą, kuri paprašytų vartotojo įvesti žodį ir atspausdintų kiek simbolių yra
+        // tame žodyje.
+    }
+}
