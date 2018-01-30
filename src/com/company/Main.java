ab065876a3c5a06d8302deeaeba94f062096b860
Zigmas <zambrazaitis@gmail.com>
2018-01-30 14:51:12 +0200

    uzduotis2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    // 1.2.​ Parašyti programą, kuri paprašytų vartotojo įvesti žodį ir atspausdintų kiek simbolių yra
+        // 1.2.​ Parašyti programą, kuri paprašytų vartotojo įvesti žodį ir atspausdintų kiek simbolių yra
         // tame žodyje.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite zodi");
+        String zodis = sc.nextLine();
+        System.out.println("Jusu zodis: " + zodis);
     }
 }
